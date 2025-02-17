4e6e66c752968257b81bf5ed6dba732b5f91e8d4
Enrico Minack <github@enrico.minack.dev>
2025-02-17 18:01:48 +0100

    Update location of failed blocks and retry if migrated

--- core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
+++ core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
@@ -979,8 +979,39 @@ final class ShuffleBlockFetcherIterator(
               log"${MDC(MAX_ATTEMPTS, maxAttemptsOnNettyOOM)} retries due to Netty OOM"
             logError(logMessage)
             errorMsg = logMessage.message
+          } else {
+            logInfo(s"Block $blockId fetch failed for mapIndex $mapIndex from $address")
           }
-          throwFetchFailedException(blockId, mapIndex, address, e, Some(errorMsg))
+
+          val newBlocksByAddr = blockId match {
+            case ShuffleBlockId(shuffleId, _, reduceId) =>
+              mapOutputTracker.getMapSizesByExecutorId(
+                shuffleId,
+                mapIndex,
+                mapIndex,
+                reduceId,
+                reduceId)
+                .filter(_._1 != address)
+            case ShuffleBlockBatchId(shuffleId, _, startReduceId, endReduceId) =>
+              mapOutputTracker.getMapSizesByExecutorId(
+                  shuffleId,
+                  mapIndex,
+                  mapIndex,
+                  startReduceId,
+                  endReduceId)
+                .filter(_._1 != address)
+            case _ =>
+              logInfo(s"Fetching block $blockId failed")
+              Iterator.empty
+          }
+          if (newBlocksByAddr.nonEmpty) {
+            logInfo(s"New addresses found for block $blockId and mapIndex $mapIndex, " +
+              s"rescheduling request: $address -> ${newBlocksByAddr.map(_._1).mkString(", ")}")
+            fallbackFetch(newBlocksByAddr)
+            result = null
+          } else {
+            throwFetchFailedException(blockId, mapIndex, address, e, Some(errorMsg))
+          }
 
         case DeferFetchRequestResult(request) =>
           val address = request.address
